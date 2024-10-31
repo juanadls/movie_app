ae1f1ce809a13eb97432015338b3752a6be36705
juanadls <eleo.dev09@gmail.com>
2024-10-31 10:11:43 -0300

    home,details, swiper card widget creation

---
+++ lib/screens/details_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreens extends StatelessWidget {
+  const DetailsScreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/widgets/card_swiper.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CardSwiper extends StatelessWidget {
+  const CardSwiper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: double.infinity,
+      height: 500,
+    );
+  }
+}
