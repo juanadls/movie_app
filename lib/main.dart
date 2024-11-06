0d7733ed35683aadbb0f2456dc33db89d92e568a
juanadls <eleo.dev09@gmail.com>
2024-11-06 18:30:52 -0300

    added provider and model

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/providers/movies_provider.dart';
 import 'package:movie_app/screens/screens.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const AppState());
+}
+
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (BuildContext context) {
+            return MoviesProvider();
+          },
+          lazy: false,
+        )
+      ],
+      child: const MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,5 +1,34 @@
-class MoviesProvider {
-  getOnDisplayMovie(){
-    
+import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+
+import 'package:http/http.dart' as http;
+
+class MoviesProvider extends ChangeNotifier {
+  final String _baseUrl = "api.themoviedb.org";
+  final String _apiKey = "86462bcf105275167165c76f625dac3d";
+  final String _language = "es-ES";
+  MoviesProvider() {
+    getOnDisplayMovies();
+  }
+  getOnDisplayMovies() async {
+    final url = Uri.https(
+      _baseUrl,
+      '/3/movie/now_playing',
+      {
+        'apiKey': _apiKey,
+        "language": _language,
+        "page": "1",
+      },
+    );
+
+    // Await the http get response, then decode the json-formatted response.
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final jsonResponse =
+          convert.jsonDecode(response.body) as Map<String, dynamic>;
+      print(response.body);
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
   }
 }
