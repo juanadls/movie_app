cda48ae5692470d1d699e4ef540dbbdbd0037731
juanadls <eleo.dev09@gmail.com>
2024-11-06 18:45:18 -0300

    added movie model

---
+++ lib/models/movie.dart
@@ -0,0 +1,76 @@
+import 'dart:convert';
+
+class Movie {
+  bool adult;
+  String? backdropPath;
+  List<int> genreIds;
+  int id;
+  OriginalLanguage originalLanguage;
+  String originalTitle;
+  String overview;
+  double popularity;
+  String? posterPath;
+  DateTime? releaseDate;
+  String title;
+  bool video;
+  double voteAverage;
+  int voteCount;
+
+  Movie({
+    required this.adult,
+    this.backdropPath,
+    required this.genreIds,
+    required this.id,
+    required this.originalLanguage,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    this.posterPath,
+    this.releaseDate,
+    required this.title,
+    required this.video,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  factory Movie.fromRawJson(String str) => Movie.fromJson(json.decode(str));
+
+  factory Movie.fromJson(Map<String, dynamic> json) => Movie(
+        adult: json["adult"],
+        backdropPath: json["backdrop_path"],
+        genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
+        id: json["id"],
+        originalLanguage:
+            originalLanguageValues.map[json["original_language"]]!,
+        originalTitle: json["original_title"],
+        overview: json["overview"],
+        popularity: json["popularity"]?.toDouble(),
+        posterPath: json["poster_path"],
+        releaseDate: DateTime.parse(json["release_date"]),
+        title: json["title"],
+        video: json["video"],
+        voteAverage: json["vote_average"]?.toDouble(),
+        voteCount: json["vote_count"],
+      );
+}
+
+enum OriginalLanguage { EN, ES, FR, JA }
+
+final originalLanguageValues = EnumValues({
+  "en": OriginalLanguage.EN,
+  "es": OriginalLanguage.ES,
+  "fr": OriginalLanguage.FR,
+  "ja": OriginalLanguage.JA
+});
+
+class EnumValues<T> {
+  Map<String, T> map;
+  late Map<T, String> reverseMap;
+
+  EnumValues(this.map);
+
+  Map<T, String> get reverse {
+    reverseMap = map.map((k, v) => MapEntry(v, k));
+    return reverseMap;
+  }
+}
