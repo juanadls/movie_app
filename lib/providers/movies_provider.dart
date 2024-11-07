accb8fa1f8fdcc1a1e63aa8923bfa72f8ac79467
juanadls <eleo.dev09@gmail.com>
2024-11-07 07:27:33 -0300

    added fromJson method

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:convert' as convert;
 
 import 'package:http/http.dart' as http;
+import 'package:movie_app/models/now_playing_response.dart';
 
 class MoviesProvider extends ChangeNotifier {
   final String _baseUrl = "api.themoviedb.org";
@@ -24,11 +25,10 @@ class MoviesProvider extends ChangeNotifier {
     // Await the http get response, then decode the json-formatted response.
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      final jsonResponse =
-          convert.jsonDecode(response.body) as Map<String, dynamic>;
-      print(response.body);
-    } else {
-      print('Request failed with status: ${response.statusCode}.');
-    }
+      if (response.body.isNotEmpty) {
+        final playingResponse =
+            NowPlayingResponse.fromJson(response.body as Map<String, dynamic>);
+      }
+    } else {}
   }
 }
